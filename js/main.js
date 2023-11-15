b60effccbdd54422aaaa6501e266ef9a82f24888
jcbarbaf <jcbarbaf@gmail.com>
2023-11-15 12:11:05 +0100

    media queries

--- js/main.js
+++ js/main.js
@@ -16,3 +16,14 @@ function CheckMessage(input) {
         input.parentNode.classList.remove("valid")
     }
 }
+function closeAsideMedia() {
+    console.log("cambio")
+    if (window.innerWidth <= 767 && !(document.getElementsByTagName("aside")[0].classList.contains("closed"))) {
+        document.getElementsByTagName("aside")[0].classList.add("closed")
+        console.log("cerrar aside")
+    }
+}
+
+// Verifica el ancho de la pantalla al cargar la página y al cambiar su tamaño
+window.onload = closeAsideMedia();
+window.onresize = closeAsideMedia();
